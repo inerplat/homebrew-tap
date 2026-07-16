dffde46c0d9bc164161efb4d405815b2bcf9ef80
donghyun-k <donghyun.k@navercorp.com>
2026-07-16 16:53:46 +0900

    fix(wirekube): update formula to v0.0.16

--- Formula/wirekube.rb
+++ Formula/wirekube.rb
@@ -1,28 +1,28 @@
 class Wirekube < Formula
   desc "Connect Kubernetes nodes across networks with a WireGuard mesh"
   homepage "https://github.com/inerplat/wirekube"
-  version "0.0.15"
+  version "0.0.16"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/inerplat/wirekube/releases/download/v0.0.15/wirekubectl-darwin-arm64"
-      sha256 "657a03b23eeb0d2cbdd6ac308d9b3cd88bb683cb0d21eb27bf9ad6698de359d3"
+      url "https://github.com/inerplat/wirekube/releases/download/v0.0.16/wirekubectl-darwin-arm64"
+      sha256 "676c61b000592644d162193cf8d0b01e1d18922362561e8934a82c47a819e116"
     end
     on_intel do
-      url "https://github.com/inerplat/wirekube/releases/download/v0.0.15/wirekubectl-darwin-amd64"
-      sha256 "5f6312b0a34a1f6af678769b7499297204256cabe50a6d4c33988727245bd5b2"
+      url "https://github.com/inerplat/wirekube/releases/download/v0.0.16/wirekubectl-darwin-amd64"
+      sha256 "b571995fc06e8e9585054e27965b98b6c591911f93623205a2c2179041aa289b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/inerplat/wirekube/releases/download/v0.0.15/wirekubectl-linux-arm64"
-      sha256 "8269c917ae5d3d27e0f68c3148cec78643e2fe82df8a9d754f57b970f7a51e03"
+      url "https://github.com/inerplat/wirekube/releases/download/v0.0.16/wirekubectl-linux-arm64"
+      sha256 "0f5f9baebab88ea9c360a0f32559453567fafb62c58cb901bebaa71ccfd22272"
     end
     on_intel do
-      url "https://github.com/inerplat/wirekube/releases/download/v0.0.15/wirekubectl-linux-amd64"
-      sha256 "97c6fa0fda2ddeaabe6a114a25cb7621fa45d81288ac378dfae81f7b84b9b8f9"
+      url "https://github.com/inerplat/wirekube/releases/download/v0.0.16/wirekubectl-linux-amd64"
+      sha256 "822ae613fdcce61298a82dd7920c31c908ad5e3327a22d48941e3938a5641fef"
     end
   end
 
